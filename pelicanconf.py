9be9e405a229e81fa14d66f670412149db1348a3
carlos <carlos@04k.lan>
2018-01-02 14:40:31 -0200

    Configure

--- pelicanconf.py
+++ pelicanconf.py
@@ -24,18 +24,21 @@ FEED_USE_SUMMARY = True
 
 # Paths
 PATH = 'content'
-#ARTICLE_URL = "{slug}"
-#ARTICLE_SAVE_AS = "{slug}.html"
-#PAGE_URL = "{slug}"
-#PAGE_SAVE_AS = "{slug}/index.html"
+
+ARTICLE_URL = "{slug}"
+ARTICLE_SAVE_AS = "{slug}.html"
+
+PAGE_URL = "{slug}"
+PAGE_SAVE_AS = "{slug}/index.html"
 
 CATEGORY_URL = "category/{slug}"
 CATEGORY_SAVE_AS = "category/{slug}/index.html"
 
 TAG_URL = "tag/{slug}"
 TAG_SAVE_AS = "tag/{slug}/index.html"
-#AUTHOR_URL = 'author/{slug}'
-#AUTHOR_SAVE_AS = 'author/{slug}/index.html'
+
+AUTHOR_URL = 'author/{slug}'
+AUTHOR_SAVE_AS = 'author/{slug}/index.html'
 
 STATIC_PATHS = [
         'themes',
