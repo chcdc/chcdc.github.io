a3ec78aff0f99eeea7b71b8d770a110dac482fcb
carlos <carlos@04k.lan>
2018-01-02 14:38:04 -0200

    Configure

--- pelicanconf.py
+++ pelicanconf.py
@@ -30,8 +30,8 @@ PATH = 'content'
 #PAGE_SAVE_AS = "{slug}/index.html"
 #CATEGORY_URL = "category/{slug}"
 #CATEGORY_SAVE_AS = "category/{slug}/index.html"
-#TAG_URL = "tag/{slug}"
-#TAG_SAVE_AS = "tag/{slug}/index.html"
+TAG_URL = "tag/{slug}"
+TAG_SAVE_AS = "tag/{slug}/index.html"
 #AUTHOR_URL = 'author/{slug}'
 #AUTHOR_SAVE_AS = 'author/{slug}/index.html'
 
